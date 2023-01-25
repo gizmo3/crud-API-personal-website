9b297b1a7c9b45f113998d228f59ad795bd4b503
Jordan James <jordanj1303@yahoo.com.au>
2023-01-25 13:48:13 +1100

    Adding html and chess/one piece api

---
+++ index.js
@@ -0,0 +1,31 @@
+fetch("https://animechan.vercel.app/api/random/anime?title=One Piece")
+            .then((response) => response.json())
+            .then((quote) => {
+              console.log(quote)
+              document.getElementById('quote').innerHTML = quote.quote
+              document.getElementById('character').innerHTML = quote.character
+
+});
+
+fetch("https://api.chess.com/pub/player/Gizmo373/stats")
+            .then((response) => response.json())
+            .then((stats) => {
+              console.log(stats)
+              document.getElementById('rapidStat').innerHTML = stats.chess_rapid.last.rating
+              document.getElementById('recordWin').innerHTML = stats.chess_rapid.record.win
+              document.getElementById('recordLoss').innerHTML = stats.chess_rapid.record.loss
+
+});
+
+fetch("https://api.chess.com/pub/puzzle")
+    .then((response) => response.json())
+    .then((puzzle) => {
+      console.log(puzzle)
+      document.getElementById('puzzleTitle').innerHTML = puzzle.title
+      document.getElementsByClassName("thumbnail")[2].src = puzzle.image;
+      document.getElementsByClassName("author")[2].href = puzzle.url;
+
+
+
+});
+  
