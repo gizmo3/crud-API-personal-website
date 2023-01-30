7fdacc0014c36caaf65cf14b2b3c5b7baba435d8
Jordan James <jordanj1303@yahoo.com.au>
2023-01-31 09:32:20 +1100

    adding social media app

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Fetch API's 
+
 fetch("https://animechan.vercel.app/api/random/anime?title=One Piece")
             .then((response) => response.json())
             .then((quote) => {
@@ -46,3 +48,64 @@ fetch("https://stoicquotesapi.com/v1/api/quotes/random")
   document.getElementById('writer').innerHTML = stoic.author
 
 })
+
+
+// Integrating Crud
+// ----------------------------------------
+
+let form = document.getElementById("form")
+let posts = document.getElementById("posts")
+let msg = document.getElementById("msg")
+let input = document.getElementById("input") //text area
+
+// adding event listener with "submit" because it is a submit button
+form.addEventListener("submit", (e) => {
+  e.preventDefault() // stops the page from refreshing after button click
+  console.log("button clicked")
+  formValidation()
+})
+
+// if its (the form) blank do this , if its filled do this
+let formValidation = () => {
+  if(input.value === ""){
+    msg.innerHTML = "Post cannot be blank"
+    console.log("L")
+  } 
+  else {
+    msg.innerHTML = ""
+    console.log("W")
+    acceptData()
+  }
+}
+
+let data = {}
+
+// collecting data from text area and putting it in data object
+let acceptData = () => {
+  data["text"] = input.value
+  console.log(data)
+  createPost()
+}
+
+let createPost = () => {
+  posts.innerHTML += 
+  `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fa-solid fa-pen-to-square"></i>
+      <i onClick="deletePost(this)" class="fa-sharp fa-solid fa-trash"></i>
+    </span>
+  </div>
+  `
+
+  input.value = ""
+}
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove()
+}
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML
+}
